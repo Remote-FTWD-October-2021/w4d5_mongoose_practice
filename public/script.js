72487681fd8fd5cc5b4f71999550ce81a0dc6005
Jaime Jacobo <jaimejacobordv@gmail.com>
2021-11-16 12:49:29 +0100

    update and delete done

--- public/script.js
+++ public/script.js
@@ -1,23 +1,49 @@
+window.onload = () => {
+  const createStudentButton = document.getElementById("create-student-button");
 
- const createStudentButton = document.getElementById("create-student-button");
+  //Event listener del botón para crear un nuevo alumno
+  if (createStudentButton) {
+    createStudentButton.addEventListener("click", async () => {
+      const name = document.getElementById("name").value;
+      const lastName = document.getElementById("lastName").value;
+      const age = document.getElementById("age").value;
+      const classroom = document.getElementById("class").value;
+      const idioma = document.getElementById("idioma").value;
 
-createStudentButton.addEventListener("click", () => {
- 
-  const name = document.getElementById("name").value;
-  const lastName = document.getElementById("lastName").value;
-  const age = document.getElementById("age").value;
-  const classroom = document.getElementById("class").value;
-  const idioma = document.getElementById("idioma").value;
+      const allInputs = { name, lastName, age, class: classroom, idioma };
 
-  const allInputs = {name, lastName, age, class: classroom, idioma};
+      await axios({
+        method: "POST",
+        url: "/new-student",
+        data: allInputs,
+      });
+      window.location.assign("/all-students");
+    });
+  }
 
-  //Ruta POST con axios
-  axios({
-    method: "POST",
-    url: "http://localhost:3000/new-student",
-    data: allInputs
-  })
+  //Event listener para eliminar un usuario
+  const deleteStudentButton = document.getElementById("delete-student");
+  const editStudentButton = document.getElementById("edit-student");
+  if (deleteStudentButton && editStudentButton) {
+    deleteStudentButton.addEventListener("click", async () => {
+      const studentId = deleteStudentButton.getAttribute("data");
+      await axios({
+        method: "DELETE",
+        url: `/student/${studentId}`,
+      });
+      window.location.assign("/all-students");
+    });
 
-  //Ruta GET con axios
-  // axios('http://localhost:3000/new-student')
-});
+    editStudentButton.addEventListener("click", async () => {
+      const studentId = editStudentButton.getAttribute("data");
+      const classroom = document.getElementById("class-edit").value;
+      const pendingBills = document.getElementById("pendingBills-edit").value;
+      await axios({
+        method: "PUT",
+        url: `/edit-student/${studentId}`,
+        data: {class: classroom, pendingBills}
+      })
+      window.location.assign(`/student/${studentId}`)
+    });
+  }
+};
--- server.js
+++ server.js
@@ -42,23 +42,27 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 //Routes
+
+//Ruta GET para renderizar el home page
 app.get('/', (req, res)=>{
   res.render('home.hbs')
 })
 
+//Ruta GET para ver a todos mis estudiantes
 app.get('/all-students', async (req, res)=>{
-  console.log(req.query) //OUTPUT de la linea 70: { name: 'Jaime', otherName: 'Alejandro', status: 'alive' }
-  const allStudents = await Student.find({}, {name: 1, lastName: 1})
+  // console.log(req.query) //OUTPUT de la linea 70: { name: 'Jaime', otherName: 'Alejandro', status: 'alive' }
+  const allStudents = await Student.find({}, {name: 1, lastName: 1, _id: 1})
 
   res.render('allStudents.hbs', {allStudents})
 })
 
+//Ruta GET para ver la info de un estudiante por su ID
 app.get('/student/:id', async (req, res)=>{
 
   try {
     const studentInfoFromDatabase = await Student.findById(
       req.params.id,
-      {name: 1, lastName: 1, age: 1, class: 1, idioma: 1}
+      {name: 1, lastName: 1, age: 1, class: 1, idioma: 1, _id: 1}
     )
     res.render('student.hbs', studentInfoFromDatabase)
   }catch(err){
@@ -66,18 +70,39 @@ app.get('/student/:id', async (req, res)=>{
   }
 })
 
+//Ruta GET para renderizar la vista de newStudent.hbs
 app.get('/new-student', (req, res)=>{
-  console.log(req.query)
   res.render('newStudent.hbs')
 })
 
+//Ruta POST para crear un nuevo estudiante
 app.post('/new-student',  async (req, res)=>{
   try{
     const createdStudent = await Student.create(req.body)
-    res.redirect('/all-students')
+    res.send(createdStudent)
   }catch(err){
     console.log(err)
   }
+})
+
+//Ruta DELETE para eliminar un alumno por su ID
+app.delete('/student/:id', async (req, res)=>{
+  try{
+    const deletedStudent = await Student.findByIdAndDelete(req.params.id)
+    res.send(deletedStudent)
+  }catch(err){
+    console.log(err)
+  }
+})
+
+//Ruta PUT para editar un alumno
+app.put('/edit-student/:id', async (req, res)=>{
+  try{
+    const updatedStudent = await Student.findByIdAndUpdate(req.params.id, req.body)
+    res.send(updatedStudent)
+  }catch(err){
+    console.log(err)
+  } 
 })
 
 
